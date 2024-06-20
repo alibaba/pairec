e175845670ea0399e2f960addc6ef25d3b9e240e
丁静 <bruceding.jing@alibaba-inc.com>
2024-06-20 08:48:03 +0800

    test complex type features for torch rec

--- algorithm/eas/easyrec_predict_test.go
+++ algorithm/eas/easyrec_predict_test.go
@@ -148,3 +148,237 @@ func TestTorchrecMutValResponseFuncDebug(t *testing.T) {
 	}
 
 }
+
+func buildDataComplexTAypeRequest() (request *easyrec.PBRequest, item_ids []string, err error) {
+	content, err := os.ReadFile("data_complex.json")
+	if err != nil {
+		return
+	}
+	data := make(map[string]any)
+	requestData := make(map[string]any)
+
+	json.Unmarshal(content, &data)
+
+	builder := easyrec.NewEasyrecRequestBuilder()
+
+	for k, v := range data {
+		if k == "item_ids" {
+			m := v.(map[string]any)
+			for _, itemid := range m["values"].([]any) {
+				builder.AddItemId(utils.ToString(itemid, ""))
+				item_ids = append(item_ids, utils.ToString(itemid, ""))
+			}
+		} else {
+			m := v.(map[string]any)
+			values := m["values"].([]any)
+			dtype := m["dtype"].(string)
+			if len(values) == 1 {
+				switch dtype {
+				case "int64":
+					builder.AddUserFeature(k, utils.ToInt64(values[0], 0))
+					requestData[k] = utils.ToInt64(values[0], 0)
+				case "list<float>":
+					vals := values[0].([]any)
+					var features []float32
+					for _, v := range vals {
+						features = append(features, utils.ToFloat32(v, 0))
+					}
+					builder.AddUserFeature(k, features)
+					requestData[k] = features
+				case "list<double>":
+					vals := values[0].([]any)
+					var features []float64
+					for _, v := range vals {
+						features = append(features, utils.ToFloat(v, 0))
+					}
+					builder.AddUserFeature(k, features)
+					requestData[k] = features
+				case "map<int,float>":
+					vals := values[0].(map[string]any)
+					features := make(map[int]float32)
+					for k, v := range vals {
+						features[utils.ToInt(k, 0)] = utils.ToFloat32(v, 0)
+					}
+					builder.AddUserFeature(k, features)
+					requestData[k] = features
+				case "map<int,int>":
+					vals := values[0].(map[string]any)
+					features := make(map[int]int)
+					for k, v := range vals {
+						features[utils.ToInt(k, 0)] = utils.ToInt(v, 0)
+					}
+					builder.AddUserFeature(k, features)
+					requestData[k] = features
+				case "map<int,int64>":
+					vals := values[0].(map[string]any)
+					features := make(map[int]int64)
+					for k, v := range vals {
+						features[utils.ToInt(k, 0)] = utils.ToInt64(v, 0)
+					}
+					builder.AddUserFeature(k, features)
+					requestData[k] = features
+				case "map<int64,string>":
+					vals := values[0].(map[string]any)
+					features := make(map[int64]string)
+					for k, v := range vals {
+						features[utils.ToInt64(k, 0)] = utils.ToString(v, "")
+					}
+					builder.AddUserFeature(k, features)
+					requestData[k] = features
+				case "string":
+					builder.AddUserFeature(k, utils.ToString(values[0], ""))
+					requestData[k] = utils.ToString(values[0], "")
+				case "list<int>":
+					vals := values[0].([]any)
+					var features []int
+					for _, v := range vals {
+						features = append(features, utils.ToInt(v, 0))
+					}
+					builder.AddUserFeature(k, features)
+					requestData[k] = features
+				case "list<string>":
+					vals := values[0].([]any)
+					var features []string
+					for _, v := range vals {
+						features = append(features, utils.ToString(v, ""))
+					}
+					builder.AddUserFeature(k, features)
+					requestData[k] = features
+				default:
+					fmt.Println(dtype, "not support")
+
+				}
+			} else {
+				switch dtype {
+				case "float":
+					var features []any
+					for _, v := range values {
+						if v == nil {
+							features = append(features, "")
+						} else {
+							features = append(features, utils.ToFloat32(v, 0))
+						}
+					}
+					builder.AddContextFeature(k, features)
+				case "int64":
+					var features []any
+					for _, v := range values {
+						if v == nil {
+							features = append(features, "")
+						} else {
+							features = append(features, utils.ToInt64(v, 0))
+						}
+					}
+					builder.AddContextFeature(k, features)
+				case "string":
+					var features []any
+					for _, v := range values {
+						if v == nil {
+							features = append(features, "")
+						} else {
+							features = append(features, utils.ToString(v, ""))
+						}
+					}
+					builder.AddContextFeature(k, features)
+				case "list<int>":
+					var features []any
+					for _, v := range values {
+						if v == nil {
+							features = append(features, "")
+						} else {
+							vals := v.([]any)
+							var feas []int
+							for _, v := range vals {
+								feas = append(feas, utils.ToInt(v, 0))
+							}
+
+							features = append(features, feas)
+						}
+					}
+					builder.AddContextFeature(k, features)
+				case "list<int64>":
+					var features []any
+					for _, v := range values {
+						if v == nil {
+							features = append(features, "")
+						} else {
+							vals := v.([]any)
+							var feas []int64
+							for _, v := range vals {
+								feas = append(feas, utils.ToInt64(v, 0))
+							}
+
+							features = append(features, feas)
+						}
+					}
+					builder.AddContextFeature(k, features)
+				default:
+					fmt.Println(dtype, "not support")
+				}
+			}
+
+		}
+	}
+
+	request = builder.EasyrecRequest()
+	buf, _ := json.Marshal(requestData)
+	fmt.Println(string(buf))
+	return
+
+}
+
+/*
+result:
+tf request
+pytorch request
+
+	map_outputs {
+	  key: "logits"
+	  value {
+	    dtype: DT_FLOAT
+	    array_shape {
+	      dim: 512
+	    }
+	    float_val: -0.016554709523916245
+	    float_val: -0.03377682343125343
+	    float_val: -0.03862795606255531
+	    float_val: -0.006319593638181686
+	    float_val: -0.03659265115857124
+	    float_val: -0.043478984385728836
+	    float_val: -0.03849106654524803
+	    float_val: -0.02703693136572838
+	    float_val: -0.04433808848261833
+	    float_val: -0.02531815692782402
+	    float_val: -0.03893169388175011
+	    float_val: -0.040057118982076645
+*/
+func TestTorchrecComplexMutValResponseFunc(t *testing.T) {
+	request, item_ids, err := buildDataComplexTAypeRequest()
+	//fmt.Println(request)
+	if err != nil {
+		t.Fatal(err)
+	}
+	pbData, _ := proto.Marshal(request)
+	client := eas.NewPredictClient("http://1730760139076263.cn-beijing.pai-eas.aliyuncs.com", "test_torch_rec_multi_tower_din_gpu")
+	client.SetToken(os.Getenv("TEST_TORCHREC_DIN_TOKEN"))
+	err = client.Init()
+	if err != nil {
+		t.Fatal(err)
+	}
+	respBody, err := client.BytesPredict(pbData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	responseData := &easyrec.TorchRecPBResponse{}
+	proto.Unmarshal(respBody, responseData)
+	responseData.ItemIds = item_ids
+
+	alogResponse, err := torchrecMutValResponseFunc(responseData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, algo := range alogResponse {
+		fmt.Println(algo.GetScoreMap())
+	}
+
+}
