e3e871e15f78c467bed9cd9525957206cdf93528
丁静 <bruceding.jing@alibaba-inc.com>
2025-11-10 15:26:47 +0800

    add expr normalizer

--- service/feature/feature_test.go
+++ service/feature/feature_test.go
@@ -18,30 +18,55 @@ func TestCreateNewFeatureOp(t *testing.T) {
 	var items []*module.Item
 	items = append(items, item1, item2)
 
-	conf := recconf.FeatureLoadConfig{}
-	conf.Features = append(conf.Features, recconf.FeatureConfig{
-		FeatureType:  "new_feature",
-		FeatureStore: "item",
-		//FeatureSource: "item:recall_name",
-		Normalizer:  "expression",
-		Expression:  "recall_name in ('retarget_u2i','realtime_retarget_click')",
-		FeatureName: "is_retarget",
+	t.Run("use expression normalizer", func(t *testing.T) {
+		conf := recconf.FeatureLoadConfig{}
+		conf.Features = append(conf.Features, recconf.FeatureConfig{
+			FeatureType:  "new_feature",
+			FeatureStore: "item",
+			//FeatureSource: "item:recall_name",
+			Normalizer:  "expression",
+			Expression:  "recall_name in ('retarget_u2i','realtime_retarget_click')",
+			FeatureName: "is_retarget",
+		})
+
+		feature := LoadWithConfig(conf)
+		feature.LoadFeatures(nil, items, context.NewRecommendContext())
+
+		for _, item := range items {
+			fmt.Println(item.Id, item.GetProperties())
+			if item.Id == "123" && item.GetProperty("is_retarget") != 0 {
+				t.Errorf("itemid:%s create new feature fail", item.Id)
+			}
+
+			if item.Id == "456" && item.GetProperty("is_retarget") != 1 {
+				t.Errorf("itemid:%s create new feature fail", item.Id)
+			}
+		}
 	})
+	t.Run("use expr normalizer", func(t *testing.T) {
+		conf := recconf.FeatureLoadConfig{}
+		conf.Features = append(conf.Features, recconf.FeatureConfig{
+			FeatureType:  "new_feature",
+			FeatureStore: "item",
+			Normalizer:   "expr",
+			Expression:   "item.recall_name in ['retarget_u2i','realtime_retarget_click']",
+			FeatureName:  "is_retarget",
+		})
 
-	feature := LoadWithConfig(conf)
-	feature.LoadFeatures(nil, items, context.NewRecommendContext())
+		feature := LoadWithConfig(conf)
+		feature.LoadFeatures(nil, items, context.NewRecommendContext())
 
-	for _, item := range items {
-		fmt.Println(item.Id, item.GetProperties())
-		if item.Id == "123" && item.GetProperty("is_retarget") != 0 {
-			t.Errorf("itemid:%s create new feature fail", item.Id)
+		for _, item := range items {
+			fmt.Println(item.Id, item.GetProperties())
+			if item.Id == "123" && item.GetProperty("is_retarget") != 0 {
+				t.Errorf("itemid:%s create new feature fail", item.Id)
+			}
+
+			if item.Id == "456" && item.GetProperty("is_retarget") != 1 {
+				t.Errorf("itemid:%s create new feature fail", item.Id)
+			}
 		}
-
-		if item.Id == "456" && item.GetProperty("is_retarget") != 1 {
-			t.Errorf("itemid:%s create new feature fail", item.Id)
-		}
-
-	}
+	})
 
 }
 
@@ -55,24 +80,85 @@ func TestCreateNewFeatureOp2(t *testing.T) {
 	var items []*module.Item
 	items = append(items, item1, item2)
 
-	conf := recconf.FeatureLoadConfig{}
-	conf.Features = append(conf.Features, recconf.FeatureConfig{
-		FeatureType:  "new_feature",
-		FeatureStore: "item",
-		Normalizer:   "expression",
-		Expression:   "getString(ali_recall_name,  recall_name)",
-		FeatureName:  "retrived",
+	t.Run("use expression normalizer", func(t *testing.T) {
+		conf := recconf.FeatureLoadConfig{}
+		conf.Features = append(conf.Features, recconf.FeatureConfig{
+			FeatureType:  "new_feature",
+			FeatureStore: "item",
+			Normalizer:   "expression",
+			Expression:   "getString(ali_recall_name,  recall_name)",
+			FeatureName:  "retrived",
+		})
+
+		feature := LoadWithConfig(conf)
+		feature.LoadFeatures(nil, items, context.NewRecommendContext())
+
+		for _, item := range items {
+			if item.Id == "123" {
+				assert.Equal(t, item.StringProperty("retrived"), "mind")
+			} else if item.Id == "456" {
+				assert.Equal(t, item.StringProperty("retrived"), "dssm")
+			}
+		}
+	})
+	t.Run("use expr normalizer", func(t *testing.T) {
+		conf := recconf.FeatureLoadConfig{}
+		conf.Features = append(conf.Features, recconf.FeatureConfig{
+			FeatureType:  "new_feature",
+			FeatureStore: "item",
+			Normalizer:   "expr",
+			Expression:   "getString(item.ali_recall_name,  item.recall_name)",
+			FeatureName:  "retrived",
+		})
+
+		feature := LoadWithConfig(conf)
+		feature.LoadFeatures(nil, items, context.NewRecommendContext())
+
+		for _, item := range items {
+			if item.Id == "123" {
+				assert.Equal(t, item.StringProperty("retrived"), "mind")
+			} else if item.Id == "456" {
+				assert.Equal(t, item.StringProperty("retrived"), "dssm")
+			}
+		}
 	})
 
-	feature := LoadWithConfig(conf)
-	feature.LoadFeatures(nil, items, context.NewRecommendContext())
+}
 
-	for _, item := range items {
-		if item.Id == "123" {
-			assert.Equal(t, item.StringProperty("retrived"), "mind")
-		} else if item.Id == "456" {
-			assert.Equal(t, item.StringProperty("retrived"), "dssm")
+func TestCreateNewFeatureOp3(t *testing.T) {
+	item1 := module.NewItem("123")
+	item1.RetrieveId = "mind"
+	item1.AddProperty("ali_recall_name", "")
+	item1.AddProperty("index", 5)
+	item2 := module.NewItem("456")
+	item2.RetrieveId = "realtime_retarget_click"
+	item2.AddProperty("ali_recall_name", "dssm")
+	item2.AddProperty("index", 8)
+	var items []*module.Item
+	items = append(items, item1, item2)
+
+	user := module.NewUser("test")
+	user.AddProperty("user_index", 13)
+	t.Run("use expr normalizer", func(t *testing.T) {
+		conf := recconf.FeatureLoadConfig{}
+		conf.Features = append(conf.Features, recconf.FeatureConfig{
+			FeatureType:  "new_feature",
+			FeatureStore: "item",
+			Normalizer:   "expr",
+			Expression:   "user.user_index - item.index",
+			FeatureName:  "index_delta",
+		})
+
+		feature := LoadWithConfig(conf)
+		feature.LoadFeatures(user, items, context.NewRecommendContext())
+
+		for _, item := range items {
+			if item.Id == "123" {
+				assert.Equal(t, item.GetProperty("index_delta"), 8)
+			} else if item.Id == "456" {
+				assert.Equal(t, item.GetProperty("index_delta"), 5)
+			}
 		}
-	}
+	})
 
 }
