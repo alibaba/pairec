7328466ea57d4fe69724f00f7be53e0188a839d8
丁静 <bruceding.jing@alibaba-inc.com>
2025-11-28 17:00:40 +0800

    add generate item data for ItemStateFilter

--- recconf/recconf.go
+++ recconf/recconf.go
@@ -707,10 +707,7 @@ type FilterConfig struct {
 	MaxItems                  int
 	TimeInterval              int // second
 	RetainNum                 int
-	ShuffleItem               bool
-	WriteLog                  bool
 	ClearLogIfNotEnoughScene  string
-	OnlyLogUserExposeFlag     bool
 	Dimension                 string
 	ScoreWeight               float64
 	GroupMinNum               int
@@ -726,7 +723,6 @@ type FilterConfig struct {
 	FilterParams              []FilterParamConfig
 	DiversityDaoConf          DiversityDaoConfig
 	DiversityMinCount         int
-	EnsureDiversity           bool
 	FilterVal                 FilterValue
 	ItemStateCacheSize        int
 	ItemStateCacheTime        int
@@ -740,6 +736,12 @@ type FilterConfig struct {
 		}
 		DefaultFilterName string
 	}
+
+	// for memory alignment
+	ShuffleItem           bool
+	WriteLog              bool
+	OnlyLogUserExposeFlag bool
+	EnsureDiversity       bool
 }
 type BeFilterConfig struct {
 	FilterConfig
@@ -749,29 +751,30 @@ type FilterValue struct {
 	WhereClause string
 }
 type SortConfig struct {
+	Name                    string
+	SortType                string
+	SortByField             string
+	SwitchThreshold         float64
+	DiversitySize           int
+	ExploreItemSize         int
+	Size                    int
+	DPPConf                 DPPSortConfig
+	SSDConf                 SSDSortConfig
+	PIDConf                 PIDControllerConfig
+	MixSortRules            []MixSortConfig
+	BoostScoreConditions    []BoostScoreCondition
+	DistinctIdConditions    []DistinctIdCondition
+	Conditions              []FilterParamConfig
+	ExcludeRecalls          []string
+	DiversityRules          []DiversityRuleConfig
+	ExclusionRules          []ExclusionRuleConfig
+	TimeInterval            int
+	BoostScoreByWeightDao   BoostScoreByWeightDaoConfig
+	MultiValueDimensionConf []MultiValueDimensionConfig
+
 	Debug                         bool
 	RemainItem                    bool
-	Name                          string
-	SortType                      string
-	SortByField                   string
-	SwitchThreshold               float64
-	DiversitySize                 int
-	ExploreItemSize               int
-	Size                          int
-	DPPConf                       DPPSortConfig
-	SSDConf                       SSDSortConfig
-	PIDConf                       PIDControllerConfig
-	MixSortRules                  []MixSortConfig
 	BoostScoreConditionsFilterAll bool
-	BoostScoreConditions          []BoostScoreCondition
-	DistinctIdConditions          []DistinctIdCondition
-	Conditions                    []FilterParamConfig
-	ExcludeRecalls                []string
-	DiversityRules                []DiversityRuleConfig
-	ExclusionRules                []ExclusionRuleConfig
-	TimeInterval                  int
-	BoostScoreByWeightDao         BoostScoreByWeightDaoConfig
-	MultiValueDimensionConf       []MultiValueDimensionConfig
 }
 
 type MultiValueDimensionConfig struct {
