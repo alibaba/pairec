5e919cceabc7c8ddbedf05cd4e89d9a790e510fe
yancheng.lgq <yancheng.lgq@alibaba-inc.com>
2025-12-11 17:34:54 +0800

    add feature base dao debug info

--- module/feature_dao.go
+++ module/feature_dao.go
@@ -104,6 +104,7 @@ type FeatureBaseDao struct {
 }
 
 func NewFeatureBaseDao(config *recconf.FeatureDaoConfig) *FeatureBaseDao {
+	log.Info(fmt.Sprintf("FeatureKey=%s\tmodule=NewFeatureBaseDaoDebug\tmsg=NewFeatureBaseDaoDebugInfo\tfeatureAppendKey(%s)", config.FeatureKey, config.FeatureAppendKey))
 	dao := FeatureBaseDao{
 		featureKey:                config.FeatureKey,
 		featureAppendKey:          config.FeatureAppendKey,
