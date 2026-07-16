049641d2ac28ab13580c40cf0c63e6740996e5fd
丁静 <bruceding.jing@alibaba-inc.com>
2026-07-16 17:03:00 +0800

    feat(fs): disable featurestore loop data loading

--- persist/fs/featurestore.go
+++ persist/fs/featurestore.go
@@ -58,6 +58,7 @@ func Load(config *recconf.RecommendConfig) {
 			featurestore.WithErrorLogger(featurestore.LoggerFunc(l.Errorf)),
 			featurestore.WithFeatureDBLogin(conf.FeatureDBUsername, conf.FeatureDBPassword),
 			featurestore.WithHologresPort(hologresPort),
+			featurestore.WithLoopData(false),
 		}
 		if conf.Domain != "" {
 			options = append(options, featurestore.WithDomain(conf.Domain))
