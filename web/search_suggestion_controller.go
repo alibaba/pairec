84a75fb9716a4a8c46c5903ec1e7a944d66e53b1
言成 <yancheng.lgq@alibaba-inc.com>
2026-08-26 14:57:14 +0800

    fix: extend standalone suggestion timeout

--- web/search_suggestion_controller.go
+++ web/search_suggestion_controller.go
@@ -26,7 +26,7 @@ import (
 const (
 	searchSuggestionBodyLimit = 16 << 10
 	searchSuggestionQueryMax  = 512
-	searchSuggestionTimeout   = 10 * time.Second
+	searchSuggestionTimeout   = 30 * time.Second
 )
 
 type SearchSuggestionParam struct {
