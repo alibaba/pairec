5164bef723d1bc365710fab64b9f2ce04eb51072
言成 <yancheng.lgq@alibaba-inc.com>
2026-09-10 16:22:10 +0800

    fix: keep shopping session keys unchanged

--- service/aishopping/session.go
+++ service/aishopping/session.go
@@ -58,7 +58,7 @@ func (s *SessionStore) Save(uid, sessionId string, blob *SessionBlob) error {
 }
 
 func sessionStoreKey(uid, sessionId string) string {
-	return "aishopping:v2:" + uid + ":" + sessionId
+	return uid + ":" + sessionId
 }
 
 func (s *SessionStore) read(sessionId string) (*SessionBlob, error) {
