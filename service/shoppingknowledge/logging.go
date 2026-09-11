e91259a59818d8f5b5556f98feff9ecfdb2f07c0
言成 <yancheng.lgq@alibaba-inc.com>
2026-09-11 11:33:28 +0800

    fix: emit model knowledge in a single log entry

--- service/shoppingknowledge/logging.go
+++ service/shoppingknowledge/logging.go
@@ -2,33 +2,15 @@ package shoppingknowledge
 
 import (
 	"fmt"
-	"unicode/utf8"
 
 	"github.com/alibaba/pairec/v2/log"
 )
-
-// Split long payloads without omitting any model-visible fields.
-const knowledgeLogChunkBytes = 4096
 
 // LogModelView logs the same knowledge JSON sent to the model.
 func (e *Evidence) LogModelView(requestID string) {
 	if e == nil {
 		return
 	}
-	payload := e.PromptJSON()
-	var chunks []string
-	for len(payload) > 0 {
-		end := min(len(payload), knowledgeLogChunkBytes)
-		if end < len(payload) {
-			for !utf8.RuneStart(payload[end]) {
-				end--
-			}
-		}
-		chunks = append(chunks, payload[:end])
-		payload = payload[end:]
-	}
-	for part, chunk := range chunks {
-		log.Info(fmt.Sprintf("requestId=%s\tmodule=ShoppingKnowledge\tevent=model_view\tpartIndex=%d\tpartCount=%d\tpayload=%s",
-			requestID, part+1, len(chunks), chunk))
-	}
+	log.Info(fmt.Sprintf("requestId=%s\tmodule=ShoppingKnowledge\tevent=model_view\tpayload=%s",
+		requestID, e.PromptJSON()))
 }
